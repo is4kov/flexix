6fc5606d087785fb675593eaf85e17973065d974
vend3tta <is4kov@gmail.com>
2015-03-05 21:46:35 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+    var Flex = {
+
+        elements: {
+            container: document.getElementById('flexbox-container')
+        },
+        options: {
+            flexDirection: 'row',
+            flexWrap: 'nowrap',
+            justifyContent: 'flex-start'
+        }
+    };
+
+    function addEl() {}
+
+    function removeEl() {}
